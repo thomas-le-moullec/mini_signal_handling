f316b55b822fe392904e58d59a838213697a6775
Thomas Le-moullec <thomas.le-moullec@epitech.eu>
2016-02-21 21:46:41 +0100

    LALA

--- includes/mine.h
+++ includes/mine.h
@@ -5,7 +5,7 @@
 ** Login   <le-mou_t@epitech.net>
 ** 
 ** Started on  Tue Feb  2 17:43:56 2016 Thomas LE MOULLEC
-** Last update Sat Feb 13 18:31:18 2016 Thomas LE MOULLEC
+** Last update Sun Feb 21 19:58:22 2016 Thomas LE MOULLEC
 */
 
 #ifndef MINE_H_
@@ -18,19 +18,16 @@
 #include <string.h>
 #include <stdio.h>
 
-/*my_lib*/
 void		my_put_nbr(int);
 int		my_get_nbr(char *);
 void		my_putchar(char);
 char		*my_strcat(char *, char *, int, int);
 char            *put_nbr_str(char *, int, int, int);
 
-/*my_assist*/
 char		*my_strcar(char *, char);
 int		my_strlen(char *);
 void            my_putstr(char *);
 
-/*client*/
 int		check_entries(int, char **);
 char		*glue_it(char *, int);
 void            put_bin(char, int);
@@ -38,7 +35,6 @@ int             ping_pong(char, int);
 void            go_ping(int);
 int             hand_it(char *, int, char **);
 
-/*serveur*/
 char		*serv_it(char *);
 void		sig_ok(int);
 int             pong_it(char *);
